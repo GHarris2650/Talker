fc6f17a2aae4e59294c1c5fe8e6e50c0aff21964
GHarris <g_harris2013@yahoo.com>
2020-12-08 20:20:55 -0600

    Changing Group names

--- public-html/group.php
+++ public-html/group.php
@@ -1,4 +1,26 @@
-<h5>Create new group</h5>
+<?php
+
+if ($_GET["gid"] != "" && phpGetGroupOwnerID($_GET["gid"]) ==$_SESSION["uid"]) {
+
+	$groupHeading = "Edit group name";
+	$hiddenField = '<input type="hidden" id="formPostsGroupID" name="formPostsGroupID" value="' . $_GET["gid"] . '">';
+
+	if ($_SESSION["group_name"] == "") {
+		$_SESSION["group_name"] = phpGetGroupName($_GET["gid"]);
+  }
+
+} else {
+
+	$groupHeading = "Create new group";
+	$hiddenField = "";
+
+}
+
+?>
+
+
+
+<h5><?php echo $groupHeading; ?></h5>
 <hr>
 
 <div class="row">
@@ -13,7 +35,9 @@
 					<?php echo (phpShowInputFeedback($_SESSION["msgid"])[1]); ?>
 					</div>
 				<?php } ?>
-			</div>
+      </div>
+      
+      <?php echo $hiddenField; ?>
 
 			<button type="submit" id="formGroupSubmit" name="formGroupSubmit" class="btn btn-primary btn-success mb-5">Submit</button>
 		</form>
